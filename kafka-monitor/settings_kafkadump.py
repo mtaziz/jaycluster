fb98744011d8277e1f5137f4388c325b5e90ae2c
cnaafhvk <308299268@qq.com>
2016-04-19 16:29:15 +0800

    取消跟踪配置文件

--- kafka-monitor/settings_kafkadump.py
+++
@@ -1,8 +0,0 @@
-################ MongoDB #####################
-MONGODB_SERVER = '192.168.200.58'
-MONGODB_PORT = 27017
-MONGODB_DB = 'products'
-
-################# Aria2 ###################
-ARIA2_ADDRESS = '192.168.200.58:6900'
-IMAGES_STORE = '/mnt/nas/pi/images_store'
