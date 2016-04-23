2e4954eca4d6fa81bf308ebc4eae952ad2552b23
cnaafhvk <308299268@qq.com>
2016-04-23 17:59:07 +0800

    不阻止超时

--- crawler/crawling/download_stats.py
+++ crawler/crawling/download_stats.py
@@ -12,32 +12,31 @@ class CostomDownloaderStats(DownloaderStats):
         reslen = len(response_httprepr(response))
         self.stats.inc_value('downloader/response_bytes', reslen, spider=spider)
         request.meta["url"] = request.url
-        if response.status not in spider.crawler.settings["RETRY_HTTP_CODES"]+[301, 302, 303, 307, 200]:
-            if request.meta.get("if_next_page"):
-                self.stats.inc_total_pages(crawlid=request.meta['crawlid'],
-                                                   spiderid=request.meta['spiderid'],
-                                                   appid=request.meta['appid'])
-            self.stats.set_failed_download_value(request.meta, response_status_message(response.status))
-        else:
-            return response
+        # if response.status not in spider.crawler.settings["RETRY_HTTP_CODES"]+[301, 302, 303, 307, 200]:
+        #     if request.meta.get("if_next_page"):
+        #         self.stats.inc_total_pages(crawlid=request.meta['crawlid'],
+        #                                            spiderid=request.meta['spiderid'],
+        #                                            appid=request.meta['appid'])
+        #     self.stats.set_failed_download_value(request.meta, response_status_message(response.status))
+        # else:
+        return response
 
     def process_exception(self, request, exception, spider):
         ex_class = "%s.%s" % (exception.__class__.__module__, exception.__class__.__name__)
         self.stats.inc_value('downloader/exception_count', spider=spider)
-        retry_times = settings.get("PROCESSING_EXCEPTION_RETRY_TIMES", 20)
-        times = request.meta.setdefault('exception_retry_times', 0)
-        if times >= retry_times:
-            request.meta["url"] = request.url
-            if request.meta.get("if_next_page"):
-                self.stats.inc_total_pages(crawlid=request.meta['crawlid'],
-                                                   spiderid=request.meta['spiderid'],
-                                                   appid=request.meta['appid'])
-            self.stats.set_failed_download_value(request.meta, ex_class)
-            self.stats.inc_value('downloader/exception_type_count/%s' % ex_class, spider=spider)
-        else:
-            retryreq = request.copy()
-            retryreq.dont_filter = True
-            retryreq.meta["exception_retry_times"] += 1
-            retryreq.meta['priority'] = retryreq.meta['priority'] - 10
-            print(">>>>>>>>>>>>>>>>>>>>>>>>"*10, times)
-            return retryreq
+        # retry_times = settings.get("PROCESSING_EXCEPTION_RETRY_TIMES", 20)
+        # times = request.meta.setdefault('exception_retry_times', 0)
+        # if times >= retry_times:
+        request.meta["url"] = request.url
+        if request.meta.get("if_next_page"):
+            self.stats.inc_total_pages(crawlid=request.meta['crawlid'],
+                                               spiderid=request.meta['spiderid'],
+                                               appid=request.meta['appid'])
+        self.stats.set_failed_download_value(request.meta, ex_class)
+        self.stats.inc_value('downloader/exception_type_count/%s' % ex_class, spider=spider)
+        # else:
+        #     retryreq = request.copy()
+        #     retryreq.dont_filter = True
+        #     retryreq.meta["exception_retry_times"] += 1
+        #     retryreq.meta['priority'] = retryreq.meta['priority'] - 10
+        #     return retryreq
