75d33ccb969196efab93e81adf3a4e8277f55e3a
cnaafhvk <308299268@qq.com>
2016-05-03 12:41:07 +0800

    修改日志显示

--- crawler/crawling/redis_retry_middleware.py
+++ crawler/crawling/redis_retry_middleware.py
@@ -7,6 +7,15 @@ class RedisRetryMiddleware(RetryMiddleware):
     def __init__(self, settings):
         RetryMiddleware.__init__(self, settings)
 
+    @classmethod
+    def from_crawler(cls, crawler):
+        cls.crawler = crawler
+        return cls(crawler.settings)
+
+    @property
+    def logger(self):
+        return self.crawler.spider._logger
+
     def _retry(self, request, reason, spider):
         retries = request.meta.get('retry_times', 0) + 1
         if retries <= self.max_retry_times:
@@ -15,7 +24,6 @@ class RedisRetryMiddleware(RetryMiddleware):
             retryreq.dont_filter = True
             # our priority setup is different from super
             retryreq.meta['priority'] = retryreq.meta['priority'] - 10
-
             return retryreq
         else:
             request.meta["url"] = request.url
@@ -24,4 +32,7 @@ class RedisRetryMiddleware(RetryMiddleware):
                                                    spiderid=request.meta['spiderid'],
                                                    appid=request.meta['appid'])
             spider.crawler.stats.set_failed_download_value(request.meta, "%s_%s"%(reason, "retry many times. "))
+            self.logger.debug("Gave up retrying %(request)s (failed %(retries)d times): %(reason)s",
+                         {'request': request, 'retries': retries, 'reason': reason},
+                         extra={'spider': spider})
             raise IgnoreRequest("max retry times")
